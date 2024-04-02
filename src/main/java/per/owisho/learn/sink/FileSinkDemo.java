8de3b38c11cb45ab8f224bae2fb191cfc3dd3bb4
Yang Wang （王洋） <yangw@opera.com>
2024-04-02 15:26:18 +0800

    add file sink demo

---
+++ src/main/java/per/owisho/learn/sink/FileSinkDemo.java
@@ -0,0 +1,46 @@
+package per.owisho.learn.sink;
+
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.serialization.SimpleStringEncoder;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.configuration.MemorySize;
+import org.apache.flink.connector.datagen.source.DataGeneratorSource;
+import org.apache.flink.connector.datagen.source.GeneratorFunction;
+import org.apache.flink.connector.file.sink.FileSink;
+import org.apache.flink.core.fs.Path;
+import org.apache.flink.streaming.api.CheckpointingMode;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.sink.filesystem.OutputFileConfig;
+import org.apache.flink.streaming.api.functions.sink.filesystem.rollingpolicies.DefaultRollingPolicy;
+
+import java.time.Duration;
+
+public class FileSinkDemo {
+
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+        env.getCheckpointConfig().setCheckpointStorage("file:///Users/owisho/Desktop/checkpoints");//注意：使用fileSink时必须开启checkpoint
+        env.enableCheckpointing(1000, CheckpointingMode.EXACTLY_ONCE);
+        DataGeneratorSource<String> genSource = new DataGeneratorSource<>(new GeneratorFunction<Long, String>() {
+            @Override
+            public String map(Long value) throws Exception {
+                return value.toString();
+            }
+        }, Long.MAX_VALUE, TypeInformation.of(String.class));
+
+        DataStreamSource<String> genStream = env.fromSource(genSource, WatermarkStrategy.noWatermarks(), "genSource");
+        FileSink<String> fileSink = FileSink
+                .<String>forRowFormat(new Path("/Users/owisho/Desktop/tmp"),
+                        new SimpleStringEncoder<String>())
+                .withOutputFileConfig(new OutputFileConfig("learn", ".log"))
+                .withRollingPolicy(DefaultRollingPolicy.builder()
+                        .withMaxPartSize(MemorySize.ofMebiBytes(1024))
+                        .withRolloverInterval(Duration.ofSeconds(3)).build()).build();
+        genStream.sinkTo(fileSink);
+        env.execute();
+
+    }
+
+}
